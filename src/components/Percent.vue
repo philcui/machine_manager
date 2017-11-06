a4ba5d49b1b4bd1ce4c37fcfa6aca131d006288c
cuixihang <cuixihang@utry.com>
2017-11-06 16:46:10 +0800

    add percent component

---
+++ src/components/Percent.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="percent">
+    <div class="inner" :style="innerStyle"></div>
+  </div>  
+</template>
+
+<script>
+export default {
+  props: {
+    percent: {
+      type: Number,
+      default: 0
+    }
+  },
+  data() {
+    return {
+      width: this.percent
+    };
+  },
+  computed:{
+    innerStyle(){
+      return {
+        width: this.percent.toString() + '%'
+      }
+    }
+  },
+  methods: {},
+  mounted() {
+    
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.percent {
+  height: 0.1rem;
+  width: 100%;
+  border-radius: 50%;
+  background-color: white;
+  border: 1px solid @divid-color;
+  .inner {
+    height: 100%;
+    background-color: @theme-color;
+    transition: width 2s;
+  }
+}
+</style>
--- src/pages/mine/PersonalCenter.vue
+++ src/pages/mine/PersonalCenter.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="personalCenter">
+    <percent :percent="percent"></percent>
     <tab-bottom :selectedItem='3'></tab-bottom>
   </div>  
 </template>
 
 <script>
 import TabBottom from "@/components/TabBottom.vue" 
+import Percent from "@/components/Percent.vue"
 export default {
   data(){
     return {
-
+      percent: 0
     }
   },
   components: {
-    TabBottom
+    TabBottom,
+    Percent
+  },
+  mounted(){
+    this.percent = 90
   }
 }
 </script>

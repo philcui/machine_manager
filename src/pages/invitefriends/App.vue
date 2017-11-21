c0ca147cc6f726a13271e462e9caae07607d7068
cuixihang <cuixihang@utry.com>
2017-11-21 23:30:47 +0800

    add new page

--- src/pages/invitefriends/App.vue
+++ src/pages/invitefriends/App.vue
@@ -1,26 +1,48 @@
 <template>
   <div id="app">
     <a href="../invitepage/index.html" class="inviteTop"></a>
-    <a href="" class="myInvite"></a>
-    <a href="" class="myPoint"></a>
-    <a href="" class="inviteBottom"></a>
+    <a href="" class="myInvite" @click.prevent="showMyInvite=true"></a>
+    <a href="" class="myPoint" @click.prevent="showGift=true"></a>
+    <a href="../invitepage/index.html" class="inviteBottom"></a>
+
+    <x-dialog v-model="showMyInvite" :dialog-style="{'background-color': 'transparent'}">
+      <div class="contentWrap">
+        <div class="num">25</div>
+      </div>
+      <div @click="showMyInvite=false">
+        <img class="close" src="../../assets/close.png" alt="">
+      </div>
+    </x-dialog>
+
+    <x-dialog v-model="showGift" :dialog-style="{'background-color': 'transparent'}">
+      <img class="toreal" src="./img/toreal.jpg" alt="">
+      <div @click="showGift=false">
+        <img class="close" src="../../assets/close.png" alt="">
+      </div>
+    </x-dialog>
   </div>
 </template>
 
 <script>
+import { XDialog } from "vux";
 export default {
   data() {
-    return {};
+    return {
+      showMyInvite: false,
+      showGift: false,
+    };
   },
   methods: {},
-  components: {}
+  components: {
+    XDialog
+  }
 };
 </script>
 
 <style lang='less'>
 @import "~vux/src/styles/reset.less";
 @import "../../style/base.less";
-//为了进度也是没办法 MDZZ PM
+//为了进度也是没办法 MDZZ
 #app {
   background: url("./img/bg_big.jpg") center center no-repeat;
   background-size: cover;
@@ -54,5 +76,29 @@ export default {
     width: 5.79rem;
     height: 0.9rem;
   }
+  .myinvite{
+    height: 7.75rem;
+  }
+  .contentWrap{
+    height: 7.75rem;
+    width: 4.74rem;
+    background: url("./img/myinvite.jpg") center center no-repeat;
+    background-size: cover;
+    position: relative;
+    .num{
+      position: absolute;
+      top: 1.44rem;
+      left: 2.05rem;
+      font-size: 0.3rem;
+      color: #e56341;
+    }
+  }
+  .toreal{
+    width: 4.8rem;
+  }
+}
+.close {
+  margin-top: 0.88rem;
+  width: 0.6rem;
 }
 </style>

5c5631a6e03b61d463f8b1f74f5a493b02fd040d
聂良思 <nieliangsi@nieliangsideMacBook-Air.local>
2017-12-16 14:59:17 +0800

    change focus

--- src/components/FocusWechat.vue
+++ src/components/FocusWechat.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="focus-wechat">
     <div class="inner">
-      <span class="slogan">找活、找人、板车、维修，请关注</span>
+      <!-- <span class="slogan">找驾驶员、故障诊断、二手机</span>
       <img class="arrow_right" src="../assets/arrow_right.png" alt="">
-      <img class="qrcode" src="../assets/qrcode.png" alt="">
+      <img class="qrcode" src="../assets/qrcode.png" alt=""> -->
+      <img src="../assets/focusGJ.png" alt="">
     </div>
   </div>  
 </template>
 
 <script>
 export default {
-
+  methods:{
+    focus(){
+      window.location.href = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU5NjA3MTA2Nw"
+    }
+  }
 }
 </script>
 
@@ -19,16 +24,17 @@ export default {
   width: 100%;
   height: 1.34rem;
   margin-top: 0.1rem;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
   .inner{
     height: 100%;
     line-height: 1.34rem;
-    border: 1px solid #434343;
+    //border: 1px solid #434343;
     width: 5.13rem;
     margin: 0 auto;
     text-align: center;
     img{
       vertical-align: middle;
+      width: 100%;
     }
     .slogan{
       color: #313131;

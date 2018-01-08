9adf78eec90f7f4d8f3bf0f2e7a77ad513986891
Sam Bai <kucubai@hotmail.com>
2018-01-08 16:46:10 +0800

    types

---
+++ src/utils/getAliasName.js
@@ -0,0 +1,10 @@
+export default function(data, target){
+  let res = data.find((item, index) => {
+    return item.value == target
+  })
+  if(res){
+    return res.alias
+  }else{
+    return ""
+  }
+}
